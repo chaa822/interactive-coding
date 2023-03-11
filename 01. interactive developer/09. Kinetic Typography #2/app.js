1898427a1c6b2dc006bfae4a2b2d3b4711ab2a36
chaa822 <chaa822@naver.com>
2023-03-11 15:57:24 +0900

    commit

---
+++ 01. interactive developer/09. Kinetic Typography #2/app.js
@@ -0,0 +1,19 @@
+
+class App {
+
+	constructor() {
+		
+		WebFont.load({
+			google: {
+				families: ['Hind:700'],
+			},
+			fontactive: () => {
+				
+			}
+		});
+	}
+}
+
+window.onload = () => {
+	new App();
+};
